13e513fa5d3d830023fe138e7836bb4b47bf13f3
Watcharapol <watcharapol.m@ku.th>
2025-09-19 22:39:12 +0700

    update google analyics

--- script.js
+++ script.js
@@ -24,4 +24,8 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 /*//////////////////////////////////////////////*/
+ window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-YBT7317T98');
